e930630acea5b7eb25f75cf3e4b67088c35247ad
Wesley Jones <wesjones2004@gmail.com>
2023-07-27 23:48:51 -1000

    first commit

---
+++ dmod-ng.cpp
@@ -0,0 +1,507 @@
+#include <dmod.hpp>
+#include <string.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+
+enum OpMode
+{
+    Compress,
+    Decompress,
+    Inspect,
+};
+
+void println(std::string msg = "");
+
+void print_help();
+
+bool contains_arg(const std::vector<std::string> &args, const std::string &arg, size_t pos = -1);
+
+OpMode parse_get_mode(const std::vector<std::string> &args);
+
+int inspect_mode(const std::vector<std::string> &args);
+int compress_mode(const std::vector<std::string> &args);
+int decompress_mode(const std::vector<std::string> &args);
+
+int main(int argc, char *argv[])
+{
+    std::vector<std::string> arguments = std::vector<std::string>(argv + 1, argv + argc);
+
+    if (arguments.size() == 0 || contains_arg(arguments, "--help") || contains_arg(arguments, "-h"))
+    {
+        print_help();
+        return 0;
+    }
+
+    struct dmod_header header;
+    u8 aes_key[32];
+    memset(aes_key, 0, 32);
+
+    u8 ivkey[16];
+    memset(ivkey, 0, 16);
+
+    dmod_header_init(&header);
+
+    struct dmod_maker_ctx ctx;
+
+    dmod_ctx_init(&ctx);
+    ctx.header = &header;
+
+    dmod_set_cipher(&ctx, DMOD_CIPHER_AES_256_CTR);
+    dmod_set_key(&ctx, aes_key);
+    dmod_set_iv(&ctx, ivkey);
+
+    // Set compress
+    dmod_set_metadata_flags(&ctx, DMOD_COMPRESSOR_ZLIB);
+
+    // Test add metadata
+    dmod_add_metadata(&ctx, "author.email", "wesjones2004@gmail.com");
+    dmod_add_metadata(&ctx, "author.name", "Wesley Jones");
+    dmod_add_metadata(&ctx, "author.website", "https://wesjones2004.github.io");
+    dmod_add_metadata(&ctx, "software.version", "0.0.1");
+    dmod_add_metadata(&ctx, "software.name", "dmod");
+    dmod_add_metadata(&ctx, "software.description", "A module format for myself");
+    dmod_add_metadata(&ctx, "software.license", "Proprietary");
+
+    dmod_header_final(ctx.header);
+
+    dmod_write(&ctx, "module.dmod");
+
+    dmod_ctx_free(&ctx);
+
+    println();
+    println();
+
+    OpMode mode = parse_get_mode(arguments);
+
+    switch (mode)
+    {
+    case Compress:
+        return compress_mode(arguments);
+    case Decompress:
+        return decompress_mode(arguments);
+    case Inspect:
+        return inspect_mode(arguments);
+    default:
+        println("Unknown mode");
+        return 1;
+    }
+
+    return 0;
+}
+
+OpMode parse_get_mode(const std::vector<std::string> &args)
+{
+    if (contains_arg(args, "--compress", 0) || contains_arg(args, "-c", 0))
+    {
+        return OpMode::Compress;
+    }
+    else if (contains_arg(args, "--decompress", 0) || contains_arg(args, "-D", 0))
+    {
+        return OpMode::Decompress;
+    }
+    else
+    {
+        return OpMode::Inspect;
+    }
+}
+
+bool contains_arg(const std::vector<std::string> &args, const std::string &arg, size_t pos)
+{
+    // If -1 search any position
+    if (pos == -1)
+    {
+        for (size_t i = 0; i < args.size(); i++)
+        {
+            if (args[i] == arg)
+            {
+                return true;
+            }
+        }
+    }
+    else
+    {
+        if (pos >= args.size())
+        {
+            return false;
+        }
+
+        if (args[pos] == arg)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void print_help()
+{
+    println("dmod-ng v0.1-dev - An extensible container format for modules");
+    println("Author: Wesley Jones <@Kracken256>");
+    println("License: Proprietary");
+    println();
+    println("Usage: dmod-ng [options] <input> <output>");
+    println("Options:");
+    println("  --help, -h: Print this help message");
+    println("  --version, -v: Print the version of dmod-ng");
+    println("  --compress, -c: Compress the module");
+    println("  --encrypt, -e: Encrypt the module");
+    println("  --decrypt, -d: Decrypt the module");
+    println("  --key, -k: Set the key for encryption/decryption");
+    println("  --sign, -s: Sign the module");
+    println("  --sign-key, -S: Set the key for signing");
+    println("  --verify, -V: Verify the module");
+    println("  --verify-key, -K: Set the key for verification");
+    println("  --decompress, -D: Decompress the module");
+
+    println("  --add-metadata, -a: Add metadata to the module");
+    println("  --remove-metadata, -r: Remove metadata from the module");
+    println("  --list-metadata, -l: List metadata from the module");
+    println("  --extract-metadata, -x: Extract metadata from the module");
+
+    println();
+}
+
+void println(std::string msg)
+{
+    std::cout << msg << std::endl;
+}
+
+std::string to_version(u16 num)
+{
+    std::string result;
+    switch (num)
+    {
+    case 0:
+        result = "Nil";
+        break;
+    case 1:
+        result = "v0.1";
+        break;
+    default:
+        result = "Unknown";
+        break;
+    }
+
+    if (num & 0x8000)
+    {
+        result += "-dev";
+    }
+
+    return result;
+}
+
+std::string to_symmetric_algorithm(u16 num)
+{
+    std::string result;
+
+    switch (num)
+    {
+    case DMOD_CIPHER_NONE:
+        result = "None";
+        break;
+    case DMOD_CIPHER_AES_128_CTR:
+        result = "AES-128-CTR (128-bit key)";
+        break;
+    case DMOD_CIPHER_AES_256_CTR:
+        result = "AES-256-CTR (256-bit key)";
+        break;
+    case DMOD_CIPHER_CHACHA20:
+        result = "ChaCha20 (256-bit key)";
+        break;
+    default:
+        result = "Unknown (" + std::to_string(num) + ")";
+        break;
+    }
+
+    return result;
+}
+
+std::string to_digest_algorithm(u16 num)
+{
+    std::string result;
+
+    switch (num)
+    {
+    case DMOD_DIGEST_ALGO_NONE:
+        result = "None";
+        break;
+    case DMOD_DIGEST_ALGO_SHA_256:
+        result = "SHA-256";
+        break;
+    case DMOD_DIGEST_ALGO_SHA_3_256:
+        result = "SHA-3-256";
+        break;
+    default:
+        result = "Unknown (" + std::to_string(num) + ")";
+        break;
+    }
+
+    return result;
+}
+
+std::string to_hexstring(const u8 *bytes, size_t len, size_t indent = 0)
+{
+    std::stringstream ss;
+    for (int i = 0; i < len; i++)
+    {
+        if (i % 10 == 0 && i != 0)
+        {
+            ss << "\n"
+               << std::string(indent, ' ');
+        }
+
+        ss << std::setw(2) << std::setfill('0') << std::hex << (int)(bytes[i]);
+
+        if (i != len - 1 && i % 10 != 9)
+        {
+            ss << ":";
+        }
+    }
+
+    return ss.str();
+}
+
+int inspect_mode(const std::vector<std::string> &args)
+{
+    if (args.size() < 1)
+    {
+        println("No input file specified");
+        return 1;
+    }
+
+    std::string input_file = args.back();
+
+    if (!std::filesystem::exists(input_file))
+    {
+        println("Input file does not exist");
+        return 1;
+    }
+
+    std::ifstream input_stream(input_file, std::ios::binary);
+
+    if (!input_stream.is_open())
+    {
+        println("Failed to open input file");
+        return 1;
+    }
+
+    struct dmod_header header;
+    bool header_valid = false;
+    bool metadata_valid = false;
+    bool crypto_valid = false;
+
+    if (input_stream.readsome((char *)&header, sizeof(struct dmod_header)) != sizeof(struct dmod_header))
+    {
+        println("Failed to read header. The file may be corrupted");
+        return 1;
+    }
+
+    // Check magic
+    if (header.preamble.magic != DMOD_PREAMBLE_MAGIC)
+    {
+        println("The magic number is incorrect. The file may be corrupted");
+        std::cout << "Expected magic value = " << std::setw(8) << std::setfill('0') << std::hex << DMOD_PREAMBLE_MAGIC << std::endl;
+        std::cout << "Actual magic value = " << std::setw(8) << std::setfill('0') << std::hex << header.preamble.magic << std::endl;
+    }
+
+    // Checksum on header preamble
+    u64 checksum = dmod_preamble_checksum((dmod_header *)&header.preamble);
+    if (checksum != header.preamble.checksum)
+    {
+        println("The checksum is incorrect. The file may be corrupted");
+        std::cout << "Expected checksum = " << std::setw(8) << std::setfill('0') << std::hex << checksum << std::endl;
+        std::cout << "Actual checksum = " << std::setw(8) << std::setfill('0') << std::hex << header.preamble.checksum << std::endl;
+    }
+    else
+    {
+        header_valid = true;
+    }
+
+    // Check version
+    if (header.preamble.version != DMOD_VERSION)
+    {
+        println("This version of dmod-ng does not support the version of this module");
+        std::cout << "Expected version = " << DMOD_VERSION << std::endl;
+        std::cout << "Actual version = " << header.preamble.version << std::endl;
+    }
+
+    // Metadata section
+    if (header.metadata.magic != DMOD_METADATA_MAGIC)
+    {
+        println("The metadata magic number is incorrect. The file may be corrupted");
+        std::cout << "Expected magic value = " << std::setw(8) << std::setfill('0') << std::hex << DMOD_METADATA_MAGIC << std::endl;
+        std::cout << "Actual magic value = " << std::setw(8) << std::setfill('0') << std::hex << header.metadata.magic << std::endl;
+    }
+
+    // Checksum on metadata
+    checksum = dmod_metadata_checksum(&header.metadata);
+    if (checksum != header.metadata.checksum)
+    {
+        println("The metadata checksum is incorrect. The file may be corrupted");
+        std::cout << "Expected checksum = " << std::setw(8) << std::setfill('0') << std::hex << checksum << std::endl;
+        std::cout << "Actual checksum = " << std::setw(8) << std::setfill('0') << std::hex << header.metadata.checksum << std::endl;
+    }
+    else
+    {
+        metadata_valid = true;
+    }
+
+    // Crypto section
+    if (header.crypto.magic != DMOD_CRYPTO_MAGIC)
+    {
+        println("The crypto magic number is incorrect. The file may be corrupted");
+        std::cout << "Expected magic value = " << std::setw(8) << std::setfill('0') << std::hex << DMOD_CRYPTO_MAGIC << std::endl;
+        std::cout << "Actual magic value = " << std::setw(8) << std::setfill('0') << std::hex << header.crypto.magic << std::endl;
+    }
+
+    // Checksum on crypto
+    checksum = dmod_crypto_checksum(&header.crypto);
+    if (checksum != header.crypto.checksum)
+    {
+        println("The crypto checksum is incorrect. The file may be corrupted");
+        std::cout << "Expected checksum = " << std::setw(8) << std::setfill('0') << std::hex << checksum << std::endl;
+        std::cout << "Actual checksum = " << std::setw(8) << std::setfill('0') << std::hex << header.crypto.checksum << std::endl;
+    }
+    else
+    {
+        crypto_valid = true;
+    }
+
+    println("Module information:");
+
+    println("  - Module header:");
+    println("    - Module Preamble:");
+    println("      - Version: " + to_version(header.preamble.version));
+    std::cout << "      - Checksum: " << std::setw(8) << std::setfill('0') << std::hex << header.preamble.checksum << std::endl;
+    println("      - Valid: " + std::string(header_valid ? "Yes" : "No"));
+    println("      - Bytes: " + to_hexstring((u8 *)&header.preamble, sizeof(struct dmod_preamble), 15));
+
+    println("    - Module Metadata:");
+    println("      - Metadata Items: " + std::to_string(header.metadata.length));
+    println("      - Offset: " + std::to_string(header.metadata.offset) + " bytes");
+    println("      - Flags:");
+
+    // Check flags
+    bool compressed = header.metadata.flags & DMOD_METADATA_COMPRESS_MASK;
+    if (compressed)
+    {
+        println("        - Compressed: Yes");
+
+        switch (header.metadata.flags & DMOD_METADATA_COMPRESS_MASK)
+        {
+        case DMOD_COMPRESSOR_LZ4:
+            println("        - Algorithm: LZ4");
+            break;
+        case DMOD_COMPRESSOR_ZSTD:
+            println("        - Algorithm: ZSTD");
+            break;
+        case DMOD_COMPRESSOR_LZ4HC:
+            println("        - Algorithm: LZ4HC");
+            break;
+        case DMOD_COMPRESSOR_ZLIB:
+            println("        - Algorithm: ZLIB");
+            break;
+        case DMOD_COMPRESSOR_LZMA:
+            println("        - Algorithm: LZMA");
+            break;
+        case DMOD_COMPRESSOR_NONE:
+            if (compressed)
+            {
+                println("        - Warning: Compressed flag is set but no compressor is specified");
+            }
+            else
+            {
+                println("        - Algorithm: None");
+            }
+            break;
+        default:
+            println("        - Warning: Unknown compressor");
+            break;
+        }
+    }
+    else
+    {
+        println("        - Compressed: No");
+    }
+
+    bool encrypted = header.metadata.flags & DMOD_METADATA_ENCRYPT;
+    if (encrypted)
+    {
+        println("        - Encrypted: Yes");
+    }
+    else
+    {
+        println("        - Encrypted: No");
+    }
+
+    std::cout << "      - Checksum: " << std::setw(8) << std::setfill('0') << std::hex << header.metadata.checksum << std::endl;
+    println("      - Valid: " + std::string(metadata_valid ? "Yes" : "No"));
+    println("      - Bytes: " + to_hexstring((u8 *)&header.metadata, sizeof(struct dmod_metadata), 15));
+
+    println("    - Module Crypto Settings:");
+    println("      - Symmetric Algorithm: " + to_symmetric_algorithm(header.crypto.sym_cipher_algo));
+    println("      - Digest Algorithm: " + to_digest_algorithm(header.crypto.digest_algo));
+
+    u8 cipher_data_cmp[sizeof(header.crypto.cipher_data)];
+    u8 digital_signature_cmp[sizeof(header.crypto.digital_signature)];
+    u8 public_key_cmp[sizeof(header.crypto.public_key)];
+    memset(cipher_data_cmp, 0, sizeof(cipher_data_cmp));
+    memset(digital_signature_cmp, 0, sizeof(digital_signature_cmp));
+    memset(public_key_cmp, 0, sizeof(public_key_cmp));
+
+    if (memcmp(header.crypto.cipher_data, cipher_data_cmp, sizeof(cipher_data_cmp)) == 0)
+    {
+        println("      - Cipher Data: None");
+    }
+    else
+    {
+        println("      - Cipher Data: " + to_hexstring(header.crypto.cipher_data, sizeof(header.crypto.cipher_data), 21));
+    }
+
+    if (memcmp(header.crypto.digital_signature, digital_signature_cmp, sizeof(digital_signature_cmp)) == 0)
+    {
+        println("      - Digital Signature: None");
+    }
+    else
+    {
+        println("      - Digital Signature: " + to_hexstring(header.crypto.digital_signature, sizeof(header.crypto.digital_signature), 27));
+    }
+
+    if (memcmp(header.crypto.public_key, public_key_cmp, sizeof(public_key_cmp)) == 0)
+    {
+        println("      - Public Key: None");
+    }
+    else
+    {
+        println("      - Public Key: " + to_hexstring(header.crypto.public_key, sizeof(header.crypto.public_key), 20));
+    }
+
+    if (header.crypto.x509_certificate_offset > 0)
+    {
+        println("      - X509 Certificate Offset: " + std::to_string(header.crypto.x509_certificate_offset) + " bytes");
+    }
+    else
+    {
+        println("      - X509 Certificate Offset: None");
+    }
+
+    std::cout << "      - Checksum: " << std::setw(8) << std::setfill('0') << std::hex << header.crypto.checksum << std::endl;
+    println("      - Valid: " + std::string(crypto_valid ? "Yes" : "No"));
+    println("      - Bytes: " + to_hexstring((u8 *)&header.crypto, sizeof(struct dmod_crypto), 15));
+
+    return 0;
+}
+
+int compress_mode(const std::vector<std::string> &args)
+{
+    return 0;
+}
+
+int decompress_mode(const std::vector<std::string> &args)
+{
+    return 0;
+}
---
+++ dmod.cpp
@@ -0,0 +1,417 @@
+#include <dmod.hpp>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <assert.h>
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/ssl.h>
+#include <zlib.h>
+
+u32 dmod_check32(const void *data, u32 n_bytes)
+{
+    u8 digest[16];
+
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+
+    const EVP_MD *md = EVP_md5();
+
+    EVP_DigestInit_ex(mdctx, md, NULL);
+    EVP_DigestUpdate(mdctx, data, n_bytes);
+    EVP_DigestFinal_ex(mdctx, digest, NULL);
+    EVP_MD_CTX_free(mdctx);
+
+    return *(u32 *)digest;
+}
+
+u32 dmod_preamble_checksum(const struct dmod_header *header)
+{
+    return dmod_check32(&header->preamble, DMOD_PREAMBLE_SIZE - 4);
+}
+
+u32 dmod_metadata_checksum(const struct dmod_metadata *data)
+{
+    return dmod_check32(data, DMOD_METADATA_SIZE - 4);
+}
+
+u32 dmod_crypto_checksum(const struct dmod_crypto *data)
+{
+    return dmod_check32(data, DMOD_CRYPTO_SIZE - 4);
+}
+
+u32 dmod_header_checksum(const struct dmod_header *header)
+{
+    return dmod_check32(header, DMOD_HEADER_SIZE - 4);
+}
+
+void dmod_header_init(struct dmod_header *header)
+{
+    header->preamble.magic = DMOD_PREAMBLE_MAGIC;
+    header->preamble.version = DMOD_VERSION;
+
+    header->metadata.magic = DMOD_METADATA_MAGIC;
+    header->metadata.flags = 0;
+    header->metadata.length = 0;
+    header->metadata.offset = 0;
+
+    header->crypto.magic = DMOD_CRYPTO_MAGIC;
+    header->crypto.sym_cipher_algo = DMOD_SYM_CIPHER_ALGO_NONE;
+    header->crypto.digest_algo = DMOD_DIGEST_ALGO_NONE;
+    header->crypto.x509_certificate_offset = 0;
+    memset(header->crypto.cipher_data, 0, sizeof(header->crypto.cipher_data));
+    memset(header->crypto.digital_signature, 0, sizeof(header->crypto.digital_signature));
+    memset(header->crypto.public_key, 0, sizeof(header->crypto.public_key));
+
+    header->entry.symbols_entry_offset = 0;
+    header->entry.text_entry_offset = 0;
+
+    memset(header->reserved, 0, sizeof(header->reserved));
+}
+
+void dmod_header_final(struct dmod_header *header)
+{
+    header->preamble.checksum = dmod_preamble_checksum(header);
+    header->metadata.checksum = dmod_metadata_checksum(&header->metadata);
+    header->crypto.checksum = dmod_crypto_checksum(&header->crypto);
+    header->header_checksum = dmod_header_checksum(header);
+}
+
+void dmod_add_metadata(dmod_maker_ctx *ctx, std::string key, std::string value)
+{
+    dmod_metadata_item item;
+    item.key = nullptr;
+    item.value = nullptr;
+    item.index = ctx->metadata_count;
+    item.keysize = key.length();
+    item.valuesize = value.length();
+    item.key = (u8 *)malloc(item.keysize + 1);
+    item.value = (u8 *)malloc(item.valuesize + 1);
+    memcpy(item.key, key.c_str(), item.keysize);
+    memcpy(item.value, value.c_str(), item.valuesize);
+    item.key[item.keysize] = 0;
+    item.value[item.valuesize] = 0;
+    ctx->metadata.push_back(item);
+    ctx->metadata_count++;
+
+    if (ctx->header->metadata.offset == 0)
+    {
+        ctx->header->metadata.offset = DMOD_HEADER_SIZE;
+    }
+
+    ctx->header->metadata.length = ctx->metadata_count;
+}
+void compress_memory(void *in_data, size_t in_data_size, std::vector<uint8_t> &out_data)
+{
+    std::vector<uint8_t> buffer;
+
+    const size_t BUFSIZE = 4096;
+    uint8_t temp_buffer[BUFSIZE];
+
+    z_stream strm;
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.next_in = reinterpret_cast<uint8_t *>(in_data);
+    strm.avail_in = in_data_size;
+    strm.next_out = temp_buffer;
+    strm.avail_out = BUFSIZE;
+
+    deflateInit(&strm, Z_BEST_COMPRESSION);
+
+    while (strm.avail_in != 0)
+    {
+        int res = deflate(&strm, Z_NO_FLUSH);
+        if (res != Z_OK)
+        {
+            // Handle error
+            deflateEnd(&strm);
+            return;
+        }
+
+        if (strm.avail_out == 0)
+        {
+            buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
+            strm.next_out = temp_buffer;
+            strm.avail_out = BUFSIZE;
+        }
+    }
+
+    int deflate_res = Z_OK;
+    while (deflate_res == Z_OK)
+    {
+        if (strm.avail_out == 0)
+        {
+            buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
+            strm.next_out = temp_buffer;
+            strm.avail_out = BUFSIZE;
+        }
+        deflate_res = deflate(&strm, Z_FINISH);
+    }
+
+    assert(deflate_res == Z_STREAM_END);
+    buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE - strm.avail_out);
+    deflateEnd(&strm);
+
+    out_data = buffer;
+}
+
+int xcrypt_buffer(const void *in, void *out, size_t len, const void *key, const void *iv, DMOD_CIPHER type)
+{
+    if (type == DMOD_CIPHER_NONE)
+    {
+        memcpy(out, in, len);
+        return 0;
+    }
+
+    if (!(type == DMOD_CIPHER_AES_128_CTR || type == DMOD_CIPHER_AES_256_CTR || type == DMOD_CIPHER_CHACHA20))
+    {
+        return 1;
+    }
+
+    EVP_CIPHER_CTX *ctx;
+    int len_out = 0;
+    int final_len_out = 0;
+
+    EVP_CIPHER *cipher_type;
+
+    switch (type)
+    {
+    case DMOD_CIPHER_AES_128_CTR:
+        cipher_type = (EVP_CIPHER *)EVP_aes_128_ctr();
+        break;
+    case DMOD_CIPHER_AES_256_CTR:
+        cipher_type = (EVP_CIPHER *)EVP_aes_256_ctr();
+        break;
+    case DMOD_CIPHER_CHACHA20:
+        cipher_type = (EVP_CIPHER *)EVP_chacha20();
+        break;
+    default:
+        return 1;
+    }
+
+    ctx = EVP_CIPHER_CTX_new();
+
+    EVP_EncryptInit(ctx, cipher_type, (const unsigned char *)key, (const unsigned char *)iv);
+
+    EVP_EncryptUpdate(ctx, (unsigned char *)out, &len_out, (const unsigned char *)in, len);
+
+    EVP_EncryptFinal(ctx, (unsigned char *)out + len_out, &final_len_out);
+
+    EVP_CIPHER_CTX_free(ctx);
+    return 0;
+}
+
+int xpress_buffer(const void *in, u8 **out, size_t len, size_t *outlen, int op, DMOD_COMPRESSOR mode)
+{
+    if (mode == DMOD_COMPRESSOR_NONE)
+    {
+        *out = (u8 *)malloc(len);
+        memcpy(*out, in, len);
+        *outlen = len;
+        return 0;
+    }
+
+    if (op != 0 && op != 1)
+    {
+        return 1;
+    }
+
+    if (!in || !out || !outlen)
+    {
+        return 1;
+    }
+
+    // Do compression
+    if (op == 0)
+    {
+        if (mode == DMOD_COMPRESSOR_ZLIB)
+        {
+            std::vector<uint8_t> buffer;
+            compress_memory((void *)in, len, buffer);
+            *out = (u8 *)malloc(buffer.size());
+            memcpy(*out, buffer.data(), buffer.size());
+            *outlen = buffer.size();
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    // Do decompression
+    if (mode == DMOD_COMPRESSOR_ZLIB)
+    {
+        std::vector<uint8_t> out_buffer;
+        z_stream strm;
+        strm.zalloc = 0;
+        strm.zfree = 0;
+        strm.next_in = (uint8_t *)in;
+        strm.avail_in = len;
+
+        // Dynamic output buffer
+        strm.next_out = NULL;
+        strm.avail_out = 0;
+
+        inflateInit(&strm);
+
+        int res = Z_OK;
+        while (res == Z_OK)
+        {
+            uint8_t buf[4096];
+            strm.next_out = buf;
+            strm.avail_out = sizeof(buf);
+
+            res = inflate(&strm, Z_NO_FLUSH);
+            assert(res != Z_STREAM_ERROR);
+
+            switch (res)
+            {
+            case Z_NEED_DICT:
+                res = Z_DATA_ERROR;
+            case Z_DATA_ERROR:
+            case Z_MEM_ERROR:
+                inflateEnd(&strm);
+                return 1;
+            }
+
+            size_t have = sizeof(buf) - strm.avail_out;
+            out_buffer.insert(out_buffer.end(), buf, buf + have);
+        }
+
+        inflateEnd(&strm);
+
+        // Allocate output buffer
+        *out = (u8 *)malloc(out_buffer.size());
+        memcpy(*out, out_buffer.data(), out_buffer.size());
+        *outlen = out_buffer.size();
+        return 0;
+    }
+
+    return 0;
+}
+
+void dmod_write(dmod_maker_ctx *ctx, std::string path)
+{
+    printf("Writing DMOD to %s\n", path.c_str());
+    FILE *outfile = fopen(path.c_str(), "wb");
+
+    ctx->metadata_offset = DMOD_HEADER_SIZE;
+
+    printf("Writing header\n");
+
+    fwrite(ctx->header, DMOD_HEADER_SIZE, 1, outfile);
+
+    printf("Writing metadata\n");
+
+    if (ctx->metadata_count > 0)
+    {
+        u8 *plaintext_buffer = nullptr;
+        size_t buffer_size = 0;
+
+        for (u32 i = 0; i < ctx->metadata_count; i++)
+        {
+            printf("Processing metadata %s : %s\n", ctx->metadata[i].key, ctx->metadata[i].value);
+            if (plaintext_buffer == nullptr)
+            {
+                plaintext_buffer = (u8 *)malloc(8);
+            }
+
+            memcpy(plaintext_buffer + buffer_size, &ctx->metadata[i].index, 4);
+            memcpy(plaintext_buffer + buffer_size + sizeof(u32), &ctx->metadata[i].keysize, 2);
+            memcpy(plaintext_buffer + buffer_size + 6, &ctx->metadata[i].valuesize, 2);
+
+            size_t new_buf_size = buffer_size + 8 + ctx->metadata[i].keysize + ctx->metadata[i].valuesize + 8;
+
+            plaintext_buffer = (u8 *)realloc(plaintext_buffer, new_buf_size);
+
+            memcpy(plaintext_buffer + buffer_size + 8, ctx->metadata[i].key, ctx->metadata[i].keysize);
+            memcpy(plaintext_buffer + buffer_size + 8 + ctx->metadata[i].keysize, ctx->metadata[i].value, ctx->metadata[i].valuesize);
+
+            buffer_size += ctx->metadata[i].keysize + ctx->metadata[i].valuesize + 8;
+        }
+
+        u8 *compressed = nullptr;
+        size_t compressed_size = 0;
+        if (ctx->header->metadata.flags & DMOD_METADATA_COMPRESS_MASK)
+            printf("Compressing metadata...\n");
+        printf("Metadata size: %lu\n", buffer_size);
+        if (xpress_buffer(plaintext_buffer, (u8 **)&compressed, buffer_size, &compressed_size, 0, (DMOD_COMPRESSOR)(ctx->header->metadata.flags & DMOD_METADATA_COMPRESS_MASK)) != 0)
+        {
+            printf("Failed to compress metadata\n");
+            exit(1);
+        }
+
+        if (ctx->header->metadata.flags & DMOD_METADATA_COMPRESS_MASK)
+            printf("Compressed metadata from %lu to %lu\n", buffer_size, compressed_size);
+
+        u8 *ciphertext = (u8 *)malloc(compressed_size);
+
+        printf("Encrypting metadata...\n");
+        if (xcrypt_buffer(compressed, ciphertext, compressed_size, ctx->enc_key, ctx->iv, (DMOD_CIPHER)ctx->header->crypto.sym_cipher_algo))
+        {
+            printf("Failed to encrypt metadata\n");
+            exit(1);
+        }
+
+        printf("Writing metadata to %u\n", ctx->metadata_offset);
+        fwrite(ciphertext, compressed_size, 1, outfile);
+
+        printf("DMOD file created. SUCCESS.\n");
+        free(plaintext_buffer);
+        free(ciphertext);
+        free(compressed);
+    }
+
+    fclose(outfile);
+}
+
+void dmod_set_cipher(dmod_maker_ctx *ctx, DMOD_CIPHER cipher)
+{
+    ctx->header->crypto.sym_cipher_algo = (u16)cipher;
+    ctx->header->metadata.flags |= DMOD_METADATA_ENCRYPT;
+}
+
+void dmod_ctx_init(dmod_maker_ctx *ctx)
+{
+    ctx->metadata_offset = 0;
+    ctx->crypto_offset = 0;
+    ctx->entry_offset = 0;
+    ctx->text_offset = 0;
+    ctx->metadata_count = 0;
+}
+
+void dmod_ctx_free(dmod_maker_ctx *ctx)
+{
+    for (u32 i = 0; i < ctx->metadata_count; i++)
+    {
+        if (ctx->metadata[i].key != NULL)
+            free(ctx->metadata[i].key);
+
+        if (ctx->metadata[i].value != NULL)
+            free(ctx->metadata[i].value);
+    }
+}
+
+void dmod_set_key(dmod_maker_ctx *ctx, const u8 *key)
+{
+    memcpy(ctx->enc_key, key, sizeof(ctx->enc_key));
+}
+
+void dmod_set_iv(dmod_maker_ctx *ctx, const u8 *iv)
+{
+    memcpy(ctx->iv, iv, sizeof(ctx->iv));
+}
+
+void dmod_set_metadata_flags(dmod_maker_ctx *ctx, u16 flags)
+{
+    ctx->header->metadata.flags |= flags;
+}
+
+void dmod_lib_init()
+{
+
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+}
